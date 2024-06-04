08745071ecfd38d8131ea0479c3f0c1482bbb44d
Vojtěch Raška <raska.vojtech@gmail.com>
2024-06-04 11:44:55 +0200

    Method for adding rating.

--- src/ratings/ratings.controller.ts
+++ src/ratings/ratings.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Session } from '@nestjs/common';
+import { RatingsService } from './ratings.service';
 
 @Controller('ratings')
-export class RatingsController {}
+export class RatingsController {
+    constructor(private readonly ratingsService: RatingsService) {}
+
+    @Post('add')
+    @HttpCode(HttpStatus.CREATED)
+    async add(
+        @Body('stars') stars: number,
+        @Body('movieId') movieId: number,
+        @Session() session: any
+    ) {
+        await this.ratingsService.addRating(Number(session.userId), movieId, stars);
+        return { success: true, message: 'Rating sucessfully added.' };
+    }
+}
--- src/ratings/ratings.service.ts
+++ src/ratings/ratings.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { db } from '../db/db';
+import { ratingsTable } from '../db/schema';
 
 @Injectable()
-export class RatingsService {}
+export class RatingsService {
+    async addRating(userId: number, movieId: number, stars: number): Promise<any> {
+        const rating = await db.insert(ratingsTable).values({userId, movieId, stars});
+        return rating;
+    }
+}
