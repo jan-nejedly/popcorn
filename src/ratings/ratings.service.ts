f8e599661e391ce9080a4154472ed4d5f2188934
Vojtěch Raška <raska.vojtech@gmail.com>
2024-06-04 17:11:24 +0200

    Antiduplicity.

--- src/ratings/ratings.service.ts
+++ src/ratings/ratings.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { db } from '../db/db';
 import { ratingsTable } from '../db/schema';
+import { and, eq } from 'drizzle-orm';
 
 @Injectable()
 export class RatingsService {
     async addRating(userId: number, movieId: number, stars: number): Promise<any> {
+        const exist = await this.checkIfRatingExists(userId, movieId);
+        
+        if (exist) return;
+        
         const rating = await db.insert(ratingsTable).values({userId, movieId, stars});
         return rating;
+    }
+
+    async checkIfRatingExists(userId: number, movieId: number): Promise<boolean> {
+        const existingRating = await db
+            .select()
+            .from(ratingsTable)
+            .where(and(eq(ratingsTable.userId, userId), eq(ratingsTable.movieId, movieId)))
+            .limit(1);
+        return !!existingRating.length;
     }
 }
