704ecc59d0741de8ca024417f26762a0df9a65d0
Vojtěch Raška <raska.vojtech@gmail.com>
2024-06-05 15:51:46 +0200

    Add method for deleting followers.

--- src/followers/followers.controller.ts
+++ src/followers/followers.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param, Redirect } from '@nestjs/common';
+import { FollowersService } from './followers.service';
 
 @Controller('followers')
-export class FollowersController {}
+export class FollowersController {
+    constructor(private readonly followersService: FollowersService) {}
+
+    @Get('delete/:followingId')
+    @HttpCode(HttpStatus.OK)
+    @Redirect('back', 302)
+    async deleteFollowing(@Param('followingId') followingId: number): Promise<{ success: boolean }> {
+        const deleted = await this.followersService.deleteFollowingById(followingId);
+        return { success: deleted };
+    }
+}
